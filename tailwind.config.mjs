a4140b43af20ad570f7b4a0c3c1d7a85a335ef81
Felix Fong <moongod101@hotmail.com>
2024-08-04 08:16:04 +0800

    feat: add fonts + basic layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'krona-one': ['Krona One', ...defaultTheme.fontFamily.sans],
+                'oswald': ['Oswald Variable', ...defaultTheme.fontFamily.sans],
+                'oxygen': ['Oxygen', ...defaultTheme.fontFamily.sans],
+            },
+            letterSpacing: {
+                'main-name': '1rem',
+            },
+            rotate: {
+                'inverted-90': '-90deg',
+            }
+        },
     },
     plugins: [],
 }
